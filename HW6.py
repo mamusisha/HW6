bc07261684cf80dba76989dbfa7a264b7dd0c698
mamusisha <g.mamulashvili01@gmail.com>
2025-09-07 14:10:42 +0400

    task_4 added

--- HW6.py
+++ HW6.py
@@ -1,40 +1,63 @@
-# task_1
+# # task_1
+#
+# def char_generator(text):
+#     for char in text:
+#         yield char
+#
+# word = "CODE"
+# for character in char_generator(word):
+#     print(character)
+#
+#
+# # task_2
+#
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# while True:
+#     try:
+#         index = int(input("enter index: "))
+#         print(f"element: {arr[index]}")
+#     except (ValueError, IndexError):
+#         print("invalid input or index out of range.")
+#     if input("continue? (y/n): ").lower() != 'y':
+#         break
+#
+#
+# # task_3
+#
+# def counter(func):
+#     func.count = 0
+#     def wrapper(*args, **kwargs):
+#         func.count += 1
+#         print(f"call: {func.count}")
+#         return func(*args, **kwargs)
+#     return wrapper
+#
+# @counter
+# def say():
+#     print("Hi")
+#
+# for _ in range(5):
+#     say()
 
-def char_generator(text):
-    for char in text:
-        yield char
 
-word = "CODE"
-for character in char_generator(word):
-    print(character)
+# task_4
 
+import logging
+logging.basicConfig(filename='game.log', level=logging.INFO,
+                   format='%(message)s')
+questions = {
+    "15 + 27?": 42,
+    "8 * 9?": 72,
+    "144 / 12?": 12,
+    "25 - 13?": 12,
+    "7 * 7": 49
+}
+score = 0
+for question, correct_answer in questions.items():
+    user_answer = int(input(f"{question} "))
+    points = 10 if user_answer == correct_answer else 0
+    score += points
+    logging.info(f"question: {question} , answer: {user_answer} -- points: {points}")
 
-# task_2
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-while True:
-    try:
-        index = int(input("enter index: "))
-        print(f"element: {arr[index]}")
-    except (ValueError, IndexError):
-        print("invalid input or index out of range.")
-    if input("continue? (y/n): ").lower() != 'y':
-        break
-
-
-# task_3
-
-def counter(func):
-    func.count = 0
-    def wrapper(*args, **kwargs):
-        func.count += 1
-        print(f"call: {func.count}")
-        return func(*args, **kwargs)
-    return wrapper
-
-@counter
-def say():
-    print("Hi")
-
-for _ in range(5):
-    say()
+logging.info(f"total score: {score}/50")
+print(f"your final score is {score} out of 50 points!")
