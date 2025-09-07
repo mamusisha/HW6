8f78bc27c77b3356ab2dec7b95d4940448ea9edf
mamusisha <g.mamulashvili01@gmail.com>
2025-09-07 15:42:47 +0400

    task_6 added

--- HW6.py
+++ HW6.py
@@ -1,5 +1,6 @@
 # task_1
 
+print("\ntask_1")
 def char_generator(text):
     for char in text:
         yield char
@@ -11,7 +12,7 @@ for character in char_generator(word):
 
 # task_2
 
-print()
+print("\ntask_2")
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 while True:
     try:
@@ -25,7 +26,7 @@ while True:
 
 # task_3
 
-print()
+print("\ntask_3")
 def counter(func):
     func.count = 0
     def wrapper(*args, **kwargs):
@@ -46,6 +47,8 @@ for _ in range(5):
 
 import logging
 
+print("\ntask_4")
+
 #game logger
 game_logger = logging.getLogger('game')
 game_logger.addHandler(logging.FileHandler('game.log'))
@@ -58,7 +61,6 @@ questions = {
     "25 - 13?": 12,
     "7 * 7": 49
 }
-print()
 score = 0
 for question, correct_answer in questions.items():
     user_answer = int(input(f"{question} "))
@@ -67,17 +69,18 @@ for question, correct_answer in questions.items():
     game_logger.info(f"question: {question} , answer: {user_answer} -- points: {points}")
 
 game_logger.info(f"total score: {score}/50")
-print(f"your final score is {score} out of 50 points!")
+print(f"your final score is {score} out of 50 points!\n")
 
 
 # task_5
+
+print("\ntask_5")
 
 #quiz logger
 quiz_logger = logging.getLogger('quiz')
 quiz_logger.addHandler(logging.FileHandler('quiz.log'))
 quiz_logger.setLevel(logging.INFO)
 
-print()
 def quiz_generator():
     questions1 = [
         "capital of france?",
@@ -92,3 +95,43 @@ def quiz_generator():
 for question in quiz_generator():
     answer = input(f"{question} :")
     quiz_logger.info(f"question: {question} - answer: {answer}")
+quiz_logger.info("\n")
+
+
+#task_6
+
+print("\ntask_6")
+
+import random
+
+rps_logger = logging.getLogger('rps')
+rps_logger.addHandler(logging.FileHandler('rps.log'))
+rps_logger.setLevel(logging.INFO)
+
+choices = ['rock', 'paper', 'scissor']
+wins = {'rock': 'scissor', 'paper': 'rock', 'scissor': 'paper'}
+player_score = 0
+computer_score = 0
+
+while player_score < 3 and computer_score < 3:
+    player = input("rock, paper, or scissor? ").lower()
+    if player not in choices:
+        print("invalid! try again.")
+        continue
+    computer = random.choice(choices)
+    if player == computer:
+        result = "it's a tie!"
+    elif wins[player] == computer:
+        result = "player wins!"
+        player_score += 1
+    else:
+        result = "computer wins!"
+        computer_score += 1
+    rps_logger.info(f"player: {player}, computer: {computer}, result: {result}")
+
+print(f"{"player" if player_score == 3 else "computer"} won the game.")
+rps_logger.info(f"{"player" if player_score == 3 else "computer"} won the game!")
+
+
+
+
