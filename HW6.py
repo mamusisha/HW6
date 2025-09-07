27a75e97148a0cae031fb9573803ddb1c9a823af
mamusisha <g.mamulashvili01@gmail.com>
2025-09-07 13:36:14 +0400

    task_2 added

--- HW6.py
+++ HW6.py
@@ -7,3 +7,16 @@ def char_generator(text):
 word = "CODE"
 for character in char_generator(word):
     print(character)
+
+
+# task_2
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+while True:
+    try:
+        index = int(input("enter index: "))
+        print(f"element: {arr[index]}")
+    except (ValueError, IndexError):
+        print("invalid input or index out of range.")
+    if input("continue? (y/n): ").lower() != 'y':
+        break
